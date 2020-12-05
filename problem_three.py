bf9cea6b2f3fd63b62318bed8ca6c79b53344877
RoseMagura <magurarm@guilford.edu>
2020-12-05 12:20:43 -0500

    Edit: Encoding mostly complete?

--- problem_three.py
+++ problem_three.py
@@ -40,35 +40,9 @@ class Node:
 
     def __repr__(self):
         return f"Node({self.get_value()}{self.char})"
-
-class Queue():
-    def __init__(self):
-        self.q = collections.deque()
-        
-    def enq(self,value):
-        self.q.appendleft(value)
-        
-    def deq(self):
-        if len(self.q) > 0:
-            return self.q.pop()
-        else:
-            return None
-    
-    def __len__(self):
-        return len(self.q)
-    
-    def empty(self):
-        return self.__len__() == 0
         
 def find_char_freq(string):
     return collections.Counter(string)
-
-# def create_heap(data):
-#     heap = []
-#     heapify(heap)
-#     for key, value in data.items():
-#         heappush(heap, (key, value))
-#     return heap
 
 class Tree():
     def __init__(self, node):
@@ -96,39 +70,6 @@ class Tree():
                 leaves.append(item)  
         return leaves
 
-    def __repr__(self):
-        level = 0
-        q = Queue()
-        visit_order = list()
-        node = self.get_root()
-        q.enq( (node,level) )
-        while(len(q) > 0):
-            node, level = q.deq()
-            if node == None:
-                visit_order.append( ("<empty>", level))
-                continue
-            visit_order.append( (node, level) )
-            if node.has_left_child():
-                q.enq( (node.get_left_child(), level +1 ))
-            else:
-                q.enq( (None, level +1) )
-
-            if node.has_right_child():
-                q.enq( (node.get_right_child(), level +1 ))
-            else:
-                q.enq( (None, level +1) )
-
-        s = "Tree\n"
-        previous_level = -1
-        for i in range(len(visit_order)):
-            node, level = visit_order[i]
-            if level == previous_level:
-                s += " | " + str(node) 
-            else:
-                s += "\n" + str(node)
-                previous_level = level        
-        return s
-
 def sort_list(list):
     return sorted(list, key = lambda x: x.value)
 
@@ -149,7 +90,6 @@ def calculate_code(cur_node, root, code=[]):
 
 def huffman_encoding(data):
     chars = find_char_freq(data)
-    # heap = create_heap(chars)
     priority_queue = list()
     for key, value in chars.items():
         priority_queue.append(Node(value, key))
@@ -174,15 +114,13 @@ def huffman_encoding(data):
     codes = dict()
  
     leaves = tree.find_leaves()
-    # for leaf in leaves:
-    #     string = str(calculate_code(leaf, node, []))
-    #     codes[leaf.char] = string
-    # print(codes)
-    # final_string = ''
-    # for d in data:
-    #     final_string += codes.get(d, '')
-    # return final_string, tree
-    return
+    for leaf in leaves:
+        string = str(calculate_code(leaf, node, []))
+        codes[leaf.char] = string
+    final_string = ''
+    for d in data:
+        final_string += codes.get(d, '')
+    return final_string, tree
 
 def huffman_decoding(data,tree):
     pass
@@ -193,13 +131,13 @@ if __name__ == "__main__":
     a_great_sentence = "BCAADDDCCACACAC"
 
     huffman_encoding(a_great_sentence)
-    # print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
-    # print ("The content of the data is: {}\n".format(a_great_sentence))
+    print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
+    print ("The content of the data is: {}\n".format(a_great_sentence))
 
-    # encoded_data, tree = huffman_encoding(a_great_sentence)
+    encoded_data, tree = huffman_encoding(a_great_sentence)
 
-    # print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
-    # print ("The content of the encoded data is: {}\n".format(encoded_data))
+    print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
+    print ("The content of the encoded data is: {}\n".format(encoded_data))
 
     # decoded_data = huffman_decoding(encoded_data, tree)
 
